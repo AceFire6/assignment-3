3f9f4b1a9e02d0d2565148b1d94164eb430145cc
Jethro Muller <JethroMuller@gmail.com>
2015-04-04 18:02:35 +0200

    Add output handling for the codeTable and the encoded string.

--- src/main.cpp
+++ src/main.cpp
@@ -12,18 +12,20 @@ using namespace std;
 class CompareNode {
 public:
     bool operator()(const MLLJET001::HuffmanNode& a, const MLLJET001::HuffmanNode& b) {
-        return a < b;
+        // If a < b. a comes after b. The largest value gets highest priority.
+        return !(a < b);
     }
 };
 
 int main(int argc, const char* argv[]) {
     if (argc < 2) {
-        cout << "Too few arguments. Required: huffmanencode <input_file> <output_file>" << endl;
+        cout << "Too few arguments." << endl;
+        cout << "Required: huffmanencode <input_file> <output_file>" << endl;
     }
+
     string inputFileName = argv[1];
     string outputFileName = argv[2];
-
-    cout << inputFileName << "\t" << outputFileName << endl;
+    vector<string> inputStrings;
 
     unordered_map<char, int> charMap;
     ifstream inFile(inputFileName);
@@ -31,6 +33,7 @@ int main(int argc, const char* argv[]) {
 
     if (inFile.is_open()) {
         while (getline(inFile, line)) {
+            inputStrings.push_back(line);
             for (char character: line) {
                 if (charMap.find(character) != charMap.end()) {
                     charMap.at(character) += 1;
@@ -39,10 +42,15 @@ int main(int argc, const char* argv[]) {
                 }
             }
         }
+        inFile.close();
+    } else {
+        cout << "Can't open file." << endl;
+        return 0;
     }
 
     priority_queue<MLLJET001::HuffmanNode, vector<MLLJET001::HuffmanNode>, CompareNode> nodeQueue;
     for (auto pair: charMap) {
+//        cout << pair.first << '\t' << pair.second << endl;
         nodeQueue.push(MLLJET001::HuffmanNode(pair.first, pair.second));
     }
 
@@ -50,21 +58,52 @@ int main(int argc, const char* argv[]) {
 //        cout << "LOOPING" << '\t' << nodeQueue.size() << endl;
         MLLJET001::HuffmanNode small1 = nodeQueue.top();
         nodeQueue.pop();
+//        cout << small1.getText() << '\t' << small1.getFrequency() << endl;
+
         MLLJET001::HuffmanNode small2 = nodeQueue.top();
         nodeQueue.pop();
+//        cout << small2.getText() << '\t' << small2.getFrequency() << endl;
 
         int totalFrequency = small1.getFrequency() + small2.getFrequency();
 
-        MLLJET001::HuffmanNode * newParent = new MLLJET001::HuffmanNode('\0', totalFrequency);
-        newParent->left = make_shared<MLLJET001::HuffmanNode>(small1);
-        newParent->right = make_shared<MLLJET001::HuffmanNode>(small2);
+        MLLJET001::HuffmanNode newParent('\0', totalFrequency);
 
-        nodeQueue.push(*newParent);
-    }
+        newParent.left = make_shared<MLLJET001::HuffmanNode>(small1);
+        newParent.right = make_shared<MLLJET001::HuffmanNode>(small2);
+
+        nodeQueue.push(newParent);
+    };
+
+    cout << "Character Count: " << nodeQueue.top().getFrequency() << endl;
+    cout << "\nCode Table:\n-----------" << endl;
 
     MLLJET001::HuffmanTree huffmanTree(nodeQueue.top());
 
-    huffmanTree.getCharMap();
+    unordered_map<char, std::string> codeTable = huffmanTree.getCharMap();
+
+    ofstream outFile(outputFileName);
+    if (outFile.is_open()) {
+        for (auto inputString : inputStrings) {
+            for (auto character : inputString) {
+                outFile << codeTable[character];
+            }
+            outFile << endl;
+        }
+        outFile.close();
+    }
+
+    size_t index = outputFileName.find('.');
+    if (index != string::npos) {
+        outputFileName = outputFileName.substr(0, index) + ".hdr";
+    }
+
+    outFile.open(outputFileName);
+    if (outFile.is_open()) {
+        outFile << codeTable.size() << endl;
+        for (auto pair : codeTable) {
+            outFile << pair.second << '\t' << pair.first << endl;
+        }
+    }
 
     return 0;
 }
