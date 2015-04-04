be9b1f4d9fccfe54fa1015f8533f4066ac5b4b61
Jethro Muller <JethroMuller@gmail.com>
2015-04-04 18:01:02 +0200

    Set up all the constructors required by HuffmanNode.

--- src/HuffmanNode.cpp
+++ src/HuffmanNode.cpp
@@ -1,9 +1,17 @@
 #include "HuffmanNode.h"
 
-char MLLJET001::HuffmanNode::getCharacter() {
+char MLLJET001::HuffmanNode::getCharacter() const {
     return character;
 }
 
-int MLLJET001::HuffmanNode::getFrequency() {
+int MLLJET001::HuffmanNode::getFrequency() const {
     return charCount;
+}
+
+void MLLJET001::HuffmanNode::setCharacter(char character) {
+    this->character = character;
+}
+
+void MLLJET001::HuffmanNode::setCharCount(int count) {
+    charCount = count;
 }
--- src/HuffmanNode.h
+++ src/HuffmanNode.h
@@ -2,6 +2,7 @@
 #define HUFFMANNODE_H
 
 #include <memory>
+#include <string>
 
 namespace MLLJET001 {
     class HuffmanNode {
@@ -12,16 +13,62 @@ namespace MLLJET001 {
     public:
         std::shared_ptr<HuffmanNode> left;
         std::shared_ptr<HuffmanNode> right;
+        std::string text;
 
+        // Default Constructor
+        HuffmanNode() : character('\0') {};
+
+        // Destructor
+//        ~HuffmanNode() { }
+
+        // Parametrized Constructor
         HuffmanNode(char character, int charCount) : character(character), charCount(charCount) {};
 
-        char getCharacter();
+        // Copy Constructor
+        HuffmanNode(const HuffmanNode & rhs) :
+                character(rhs.character), charCount(rhs.charCount), left(rhs.left), right(rhs.right) {};
 
-        int getFrequency();
+        // Move Constructor
+        HuffmanNode(HuffmanNode && rhs) : character(rhs.character), charCount(rhs.charCount),
+                                          left(std::move(rhs.left)), right(std::move(rhs.right)) {};
+
+        // Copy Assignment
+        HuffmanNode & operator=(const HuffmanNode & rhs) {
+            if (this != &rhs) {
+                character = rhs.character;
+                charCount = rhs.charCount;
+
+                left = rhs.left;
+                right = rhs.right;
+            }
+            return *this;
+        }
+
+        // Move Assignment
+        HuffmanNode & operator=(HuffmanNode && rhs) {
+            if (this != &rhs) {
+                character = rhs.character;
+                rhs.setCharacter('\0');
+                charCount = rhs.charCount;
+                rhs.setCharCount(0);
+
+                left = std::move(rhs.left);
+                right = std::move(rhs.right);
+            }
+            return *this;
+        }
 
         bool operator<(const HuffmanNode node) const {
             return (this->charCount < node.charCount);
         }
+
+        char getCharacter() const;
+
+        int getFrequency() const;
+
+        void setCharacter(char character);
+
+        void setCharCount(int count);
     };
 }
 
